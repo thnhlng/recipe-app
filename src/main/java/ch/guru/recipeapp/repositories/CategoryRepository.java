2fefe45cd604ca9c48e62fa6eec4d93c1e041cff
thnhlng <thanh-long10@hotmail.com>
2023-05-15 17:44:08 +0200

    140. spring jpa query methods

--- src/main/java/ch/guru/recipeapp/repositories/CategoryRepository.java
+++ src/main/java/ch/guru/recipeapp/repositories/CategoryRepository.java
@@ -4,5 +4,10 @@ import ch.guru.recipeapp.domain.Category;
 import ch.guru.recipeapp.domain.Recipe;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+
 }
--- src/main/java/ch/guru/recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/ch/guru/recipeapp/repositories/UnitOfMeasureRepository.java
@@ -4,5 +4,9 @@ import ch.guru.recipeapp.domain.Recipe;
 import ch.guru.recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
